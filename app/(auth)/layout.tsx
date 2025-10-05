a435b47e7d175d09153bda22836a17a41a9260a9
Rishabh <rishabh.sharma3@appinventiv.com>
2025-10-05 16:59:55 +0530

    initiated auth flow

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Layout = ({ children } : {children : React.ReactNode}) => {
+    return (
+        <main className="min-h-screen text-gray-400">
+
+        </main>
+    )
+}
+export default Layout
